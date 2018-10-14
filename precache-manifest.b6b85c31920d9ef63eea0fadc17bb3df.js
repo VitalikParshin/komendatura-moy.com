0d6ee5973644837090acfdbf2b0c0fa3938c80c2
Vitalik Parshyn <vitalik.parshin@gmail.com>
2018-10-14 23:35:35 +0300

    Updates

--- precache-manifest.b6b85c31920d9ef63eea0fadc17bb3df.js
+++ precache-manifest.b6b85c31920d9ef63eea0fadc17bb3df.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/komendatura-moy.com/static/js/runtime~main.c23c87d3.js"
   },
   {
-    "revision": "8ca86c8219d54dadb469",
-    "url": "/komendatura-moy.com/static/js/main.8ca86c82.chunk.js"
+    "revision": "8c66a78b0184f0a70d42",
+    "url": "/komendatura-moy.com/static/js/main.8c66a78b.chunk.js"
   },
   {
     "revision": "7eae6140ad0adf38fbdc",
     "url": "/komendatura-moy.com/static/js/1.7eae6140.chunk.js"
   },
   {
-    "revision": "8ca86c8219d54dadb469",
+    "revision": "8c66a78b0184f0a70d42",
     "url": "/komendatura-moy.com/static/css/main.f89b7b18.chunk.css"
   },
   {
-    "revision": "4733457cd87aa8103426f16037afa80c",
+    "revision": "d0b616865b6e452efb10c871ad55c0b6",
     "url": "/komendatura-moy.com/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/komendatura-moy.com/precache-manifest.59540e3c1c2fadaeb2d5b55f618908dd.js"
+  "/komendatura-moy.com/precache-manifest.b6b85c31920d9ef63eea0fadc17bb3df.js"
 );
 
 workbox.clientsClaim();
