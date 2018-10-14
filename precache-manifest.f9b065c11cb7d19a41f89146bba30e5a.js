af3447275009992a4c7c766b647b62dfaca06587
Vitalik Parshyn <vitalik.parshin@gmail.com>
2018-10-14 23:20:40 +0300

    Updates

---
+++ precache-manifest.f9b065c11cb7d19a41f89146bba30e5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c23c87d34876299459df",
+    "url": "/komendatura-moy.com/static/js/runtime~main.c23c87d3.js"
+  },
+  {
+    "revision": "8ca86c8219d54dadb469",
+    "url": "/komendatura-moy.com/static/js/main.8ca86c82.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/komendatura-moy.com/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "8ca86c8219d54dadb469",
+    "url": "/komendatura-moy.com/static/css/main.f89b7b18.chunk.css"
+  },
+  {
+    "revision": "9ffae6cfe8426b01e20837e1ad5efcc5",
+    "url": "/komendatura-moy.com/index.html"
+  }
+];
